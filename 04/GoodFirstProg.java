a50758f57f9bf8b01329a0b1a6e8d5a9ffd6d304
jorgesumi <36480903+jorgesumi@users.noreply.github.com>
2018-02-15 11:05:51 -0500

    Add files via upload

---
+++ 04/GoodFirstProg.java
@@ -0,0 +1,8 @@
+public class GoodFirstProg {
+	public static void main (String[]args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello Again");
+		System.out.println("Bye!");
+	}
+}
+	
---
+++ 05/CommentsAndSlashes.java~
@@ -0,0 +1,6 @@
+public class CommentsAndSlashes {
+	public static void main (String[]args){
+		//slashes turn things into comments
+		System.out.println("two slashes make a something a comment");
+	}
+}
