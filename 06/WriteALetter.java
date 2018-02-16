bc545a7d97f2ac53f26706340fe71b9bf4b51dfb
jorgesumi <36480903+jorgesumi@users.noreply.github.com>
2018-02-16 11:09:54 -0500

    Add files via upload

---
+++ 06/WriteALetter.java
@@ -0,0 +1,17 @@
+public class WriteALetter 
+{
+	public static void main(String[]args) {
+		System.out.println("+---------------------------------------------------------+");
+		System.out.println("|                                                  ###### |");
+		System.out.println("|                                                  ###### |");
+		System.out.println("|                                                  ###### |");
+		System.out.println("|                                                  ###### |");
+		System.out.println("|                                                         |");
+		System.out.println("|                                                         |");
+		System.out.println("|                            Jorge Sumi                   |");
+		System.out.println("|                            178 Kirk Drive               |");
+		System.out.println("|                            Thornhill, On 3L4            |");
+		System.out.println("|                                                         |");
+		System.out.println("+---------------------------------------------------------+");
+	}
+}
