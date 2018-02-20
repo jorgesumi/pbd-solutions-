0ec7e92e4010d50f42839cc3fbad9d72e5b0e920
jorgesumi <36480903+jorgesumi@users.noreply.github.com>
2018-02-20 00:18:00 -0500

    Add files via upload

---
+++ 07/MyInitials.java
@@ -0,0 +1,11 @@
+public class MyInitials {
+	public static void main (String[]Args) {
+		System.out.println ("JJJJJ   SSS");
+		System.out.println ("  J    S    S");
+		System.out.println ("  J    S   ");
+		System.out.println ("  J     SSS");
+		System.out.println ("J J        S");
+		System.out.println ("J J    S   S");
+		System.out.println ("JJJ     SSS");	
+	}
+}
